dfb7a0b9ba442d9ac2b0bf067c093a07167ec775
MaulingMonkey <git@maulingmonkey.com>
2020-11-21 01:05:15 -0800

    examples

---
+++ examples/00-clear-winit.rs
@@ -0,0 +1,75 @@
+#![windows_subsystem = "windows"]
+
+use thin3d9::*;
+
+use raw_window_handle::*;
+use raw_window_handle::windows::*;
+
+use winapi::shared::d3d9::*;
+use winapi::shared::d3d9caps::*;
+use winapi::shared::d3d9types::*;
+use winapi::shared::winerror::SUCCEEDED;
+
+use winapi::um::debugapi::*;
+
+use winit::dpi::*;
+use winit::event::*;
+use winit::event_loop::*;
+use winit::window::*;
+
+use std::ptr::*;
+
+
+
+fn main() {
+    std::panic::set_hook(Box::new(|pi| unsafe {
+        eprintln!("{}", pi);
+        if IsDebuggerPresent() != 0 { DebugBreak(); }
+        std::process::exit(1);
+    }));
+
+    let event_loop  = EventLoop::new();
+    let window      = WindowBuilder::new()
+        .with_title("00-clear-winit - thin3d9 example")
+        .with_inner_size(Size::Physical(PhysicalSize { width: 800, height: 600 }))
+        .build(&event_loop).unwrap();
+    let d3d         = unsafe { Direct3D::create(SdkVersion::default()) }.unwrap();
+
+    let hwnd = match window.raw_window_handle() {
+        RawWindowHandle::Windows(WindowsHandle { hwnd, .. }) => hwnd.cast(),
+        other => panic!("Expected RawWindowHandle::Windows(...), got {:?} instead", other),
+    };
+
+    let mut pp = D3DPRESENT_PARAMETERS {
+        Windowed:               true.into(),
+        hDeviceWindow:          hwnd,
+        SwapEffect:             D3DSWAPEFFECT_DISCARD,
+        PresentationInterval:   D3DPRESENT_INTERVAL_ONE,
+        .. unsafe { std::mem::zeroed() }
+    };
+
+    // https://docs.microsoft.com/en-us/windows/win32/direct3d9/d3dcreate
+    let behavior =
+        //D3DCREATE_DISABLE_PRINTSCREEN | // not implemented by winapi?
+        D3DCREATE_FPU_PRESERVE |
+        D3DCREATE_HARDWARE_VERTEXPROCESSING |
+        D3DCREATE_NOWINDOWCHANGES;
+
+    let device = unsafe { d3d.create_device(0, DevType::HAL, null_mut(), behavior, &mut pp) }.unwrap();
+
+    event_loop.run(move |event, _, control_flow|{
+        *control_flow = ControlFlow::Poll;
+
+        match event {
+            Event::WindowEvent { event: WindowEvent::CloseRequested, window_id } if window_id == window.id() => {
+                std::process::exit(0); // device must outlast HWND which is about to be destroyed - kill the process instead
+            },
+            Event::MainEventsCleared => {
+                let color : D3DCOLOR = 0xFF224466; // ARGB
+                assert!(SUCCEEDED(unsafe { (*device.as_raw()).Clear(0, null(), D3DCLEAR_TARGET, color, 0.0, 0) }));
+                assert!(SUCCEEDED(unsafe { (*device.as_raw()).Present(null(), null(), null_mut(), null()) }));
+            },
+            _ => {},
+        }
+    });
+}
---
+++ examples/01-clear-winapi.rs
@@ -0,0 +1,114 @@
+#![windows_subsystem = "windows"]
+
+use thin3d9::*;
+
+use wchar::wch_c;
+
+use winapi::shared::d3d9::*;
+use winapi::shared::d3d9caps::*;
+use winapi::shared::d3d9types::*;
+use winapi::shared::minwindef::*;
+use winapi::shared::windef::*;
+use winapi::shared::winerror::SUCCEEDED;
+
+use winapi::um::debugapi::*;
+use winapi::um::libloaderapi::*;
+use winapi::um::winuser::*;
+
+use std::cell::RefCell;
+use std::ptr::*;
+
+
+
+thread_local! {
+    static D3D      : Direct3D = unsafe { Direct3D::create(SdkVersion::default()) }.unwrap();
+    static DEVICE   : RefCell<Option<Device>> = RefCell::new(None);
+}
+
+unsafe extern "system" fn window_proc(hwnd: HWND, umsg: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+    match umsg {
+        WM_DESTROY => {
+            DEVICE.with(|d| *d.borrow_mut() = None);
+            PostQuitMessage(0);
+            0
+        },
+        _ => DefWindowProcW(hwnd, umsg, wparam, lparam),
+    }
+}
+
+fn main() {
+    std::panic::set_hook(Box::new(|pi| unsafe {
+        eprintln!("{}", pi);
+        if IsDebuggerPresent() != 0 { DebugBreak(); }
+        std::process::exit(1);
+    }));
+
+    let hinstance = unsafe { GetModuleHandleW(null()) };
+    assert!(!hinstance.is_null());
+
+    let hcursor = unsafe { LoadCursorW(null_mut(), IDC_ARROW) };
+    assert!(!hcursor.is_null());
+
+    let wc = WNDCLASSW {
+        lpfnWndProc:    Some(window_proc),
+        hInstance:      hinstance,
+        hCursor:        hcursor,
+        lpszClassName:  wch_c!("SampleWndClass").as_ptr(),
+        .. unsafe { std::mem::zeroed() }
+    };
+    assert_ne!(0, unsafe { RegisterClassW(&wc) });
+
+    let hwnd = unsafe { CreateWindowExW(
+        0,
+        wch_c!("SampleWndClass").as_ptr(),
+        wch_c!("00-clear-winapi - thin3d9 example").as_ptr(),
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        800,
+        600,
+        null_mut(),
+        null_mut(),
+        hinstance,
+        null_mut(),
+    )};
+    assert!(!hwnd.is_null());
+    assert_eq!(0, unsafe { ShowWindow(hwnd, SW_SHOW) });
+
+    let mut pp = D3DPRESENT_PARAMETERS {
+        Windowed:               true.into(),
+        hDeviceWindow:          hwnd,
+        SwapEffect:             D3DSWAPEFFECT_DISCARD,
+        PresentationInterval:   D3DPRESENT_INTERVAL_ONE,
+        .. unsafe { std::mem::zeroed() }
+    };
+
+    // https://docs.microsoft.com/en-us/windows/win32/direct3d9/d3dcreate
+    let behavior =
+        //D3DCREATE_DISABLE_PRINTSCREEN | // not implemented by winapi?
+        D3DCREATE_FPU_PRESERVE |
+        D3DCREATE_HARDWARE_VERTEXPROCESSING |
+        D3DCREATE_NOWINDOWCHANGES;
+
+    let device = D3D.with(|d3d| unsafe { d3d.create_device(0, DevType::HAL, null_mut(), behavior, &mut pp) }).unwrap();
+    DEVICE.with(|d| *d.borrow_mut() = Some(device));
+
+    loop {
+        let mut msg = MSG { message: 0, hwnd: null_mut(), time: 0, pt: POINT { x: 0, y: 0 }, lParam: 0, wParam: 0 };
+        while unsafe { PeekMessageW(&mut msg, null_mut(), 0, 0, PM_REMOVE) } != 0 {
+            if msg.message == WM_QUIT { return; }
+            unsafe { TranslateMessage(&msg) };
+            unsafe { DispatchMessageW(&msg) };
+        }
+
+        DEVICE.with(|d| {
+            let device = d.borrow();
+            if let Some(device) = device.as_ref() {
+                let device = unsafe { &*device.as_raw() };
+                let color : D3DCOLOR = 0xFF664422; // ARGB
+                assert!(SUCCEEDED(unsafe { device.Clear(0, null(), D3DCLEAR_TARGET, color, 0.0, 0) }));
+                assert!(SUCCEEDED(unsafe { device.Present(null(), null(), null_mut(), null()) }));
+            }
+        });
+    }
+}
