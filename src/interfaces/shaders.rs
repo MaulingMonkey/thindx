cc5ca762e8d267cad298675788731d078084fea8
MaulingMonkey <git@maulingmonkey.com>
2020-11-21 20:31:48 -0800

    Device::create_vertex_shader

--- src/interfaces/shaders.rs
+++ src/interfaces/shaders.rs
@@ -42,6 +42,31 @@ impl Device {
         MethodError::check("IDirect3DDevice9::CreatePixelShader", hr)?;
         Ok(PixelShader::from_raw(shader))
     }
+
+    /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/d3d9/nf-d3d9-idirect3ddevice9-createvertexshader)\]
+    /// IDirect3DDevice9::CreateVertexShader
+    ///
+    /// Creates a vertex shader.
+    ///
+    /// ### Safety
+    ///
+    /// The caller must pass a valid shader blob.
+    /// The underlying Direct3D API is unsound - it doesn't even take a length for the DWORD array.
+    /// This function will likely attempt to validate the shader blob bytecode in the future and/or offload such validation onto the parameter, but until then this is unsound as heck.
+    /// Do not trust user-generated-content for shader bytecode blobs.
+    ///
+    /// ### Returns
+    ///
+    /// *   [D3DERR::INVALIDCALL]
+    /// *   [D3DERR::OUTOFVIDEOMEMORY]
+    /// *   [D3DERR::OUTOFMEMORY]
+    /// *   Ok([VertexShader])
+    pub unsafe fn create_vertex_shader(&self, function: &[u32]) -> Result<VertexShader, MethodError> {
+        let mut shader = null_mut();
+        let hr = self.0.CreateVertexShader(function.as_ptr(), &mut shader);
+        MethodError::check("IDirect3DDevice9::CreateVertexShader", hr)?;
+        Ok(VertexShader::from_raw(shader))
+    }
 }
 
 // #[test] fn create_pixel_shader() {} // TODO
