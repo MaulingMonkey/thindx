2ef576553909f9d9aa31ab350cdf6b8c6e7039da
MaulingMonkey <git@maulingmonkey.com>
2020-11-22 20:13:08 -0800

    Device::{get,set}_{pixel,vertex}_shader

--- src/interfaces/shaders.rs
+++ src/interfaces/shaders.rs
@@ -68,6 +68,72 @@ impl Device {
         MethodError::check("IDirect3DDevice9::CreateVertexShader", hr)?;
         Ok(VertexShader::from_raw(shader))
     }
+
+    /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/d3d9helper/nf-d3d9helper-idirect3ddevice9-setpixelshader)\]
+    /// IDirect3DDevice9::SetPixelShader
+    ///
+    /// Sets the pixel shader to render with.
+    ///
+    /// ### Returns
+    ///
+    /// *   [D3DERR::INVALIDCALL]   If `pixel_shader` was created by another device?
+    /// *   Ok(())
+    pub fn set_pixel_shader<'sh>(&self, pixel_shader: impl Into<Option<&'sh PixelShader>>) -> Result<(), MethodError> {
+        let pixel_shader = pixel_shader.into();
+        let ps = pixel_shader.map_or(null_mut(), |ps| ps.as_raw());
+        let hr = unsafe { self.0.SetPixelShader(ps) };
+        MethodError::check("IDirect3DDevice9::SetPixelShader", hr)
+    }
+
+    /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/d3d9helper/nf-d3d9helper-idirect3ddevice9-setvertexshader)\]
+    /// IDirect3DDevice9::SetVertexShader
+    ///
+    /// Sets the vertex shader to render with.
+    ///
+    /// ### Returns
+    ///
+    /// *   [D3DERR::INVALIDCALL]   If `vertex_shader` was created by another device?
+    /// *   Ok(())
+    pub fn set_vertex_shader<'sh>(&self, vertex_shader: impl Into<Option<&'sh VertexShader>>) -> Result<(), MethodError> {
+        let vertex_shader = vertex_shader.into();
+        let ps = vertex_shader.map_or(null_mut(), |ps| ps.as_raw());
+        let hr = unsafe { self.0.SetVertexShader(ps) };
+        MethodError::check("IDirect3DDevice9::SetVertexShader", hr)
+    }
+
+    /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/d3d9helper/nf-d3d9helper-idirect3ddevice9-getpixelshader)\]
+    /// IDirect3DDevice9::GetPixelShader
+    ///
+    /// Gets the pixel shader currently bound to the device, if any.
+    ///
+    /// ### Returns
+    ///
+    /// *   [D3DERR::INVALIDCALL]       If the device was created with D3DCREATE_PUREDEVICE?
+    /// *   Ok(Some([PixelShader]))     If a pixel shader was bound
+    /// *   Ok(None)                    If no pixel shader was bound
+    pub fn get_pixel_shader(&self) -> Result<PixelShader, MethodError> {
+        let mut shader = null_mut();
+        let hr = unsafe { self.0.GetPixelShader(&mut shader) };
+        MethodError::check("IDirect3DDevice9::GetPixelShader", hr)?;
+        Ok(unsafe { PixelShader::from_raw(shader) })
+    }
+
+    /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/d3d9helper/nf-d3d9helper-idirect3ddevice9-getvertexshader)\]
+    /// IDirect3DDevice9::GetVertexShader
+    ///
+    /// Gets the vertex shader currently bound to the device, if any.
+    ///
+    /// ### Returns
+    ///
+    /// *   [D3DERR::INVALIDCALL]       If the device was created with D3DCREATE_PUREDEVICE?
+    /// *   Ok(Some([VertexShader]))    If a vertex shader was bound
+    /// *   Ok(None)                    If no vertex shader was bound
+    pub fn get_vertex_shader(&self) -> Result<VertexShader, MethodError> {
+        let mut shader = null_mut();
+        let hr = unsafe { self.0.GetVertexShader(&mut shader) };
+        MethodError::check("IDirect3DDevice9::GetVertexShader", hr)?;
+        Ok(unsafe { VertexShader::from_raw(shader) })
+    }
 }
 
 // #[test] fn create_pixel_shader() {} // TODO
