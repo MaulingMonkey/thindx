4a329d8b19b87b69aa06ef0e4db8cafab4486cc1
MaulingMonkey <git@maulingmonkey.com>
2020-11-22 23:56:25 -0800

    LightType:  links into UWP graphics concepts

--- src/enums/light_type.rs
+++ src/enums/light_type.rs
@@ -9,7 +9,7 @@ use std::fmt::{self, Debug, Formatter};
 /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/direct3d9/d3dlighttype)\]
 /// D3DLIGHTTYPE
 ///
-/// Defines the light type.
+/// Defines the [light type](https://docs.microsoft.com/en-us/windows/uwp/graphics-concepts/light-types).
 ///
 /// Directional lights are slightly faster than point light sources, but point lights look a little better.
 /// Spotlights offer interesting visual effects but are computationally time-consuming.
@@ -30,16 +30,22 @@ impl LightType {
 
 #[allow(non_upper_case_globals)] // These are enum-like
 impl LightType {
-    /// Light is a point source. The light has a position in space and radiates light in all directions.
+    /// Light is a [point] source. The light has a position in space and radiates light in all directions.
+    ///
+    /// [point]:        https://docs.microsoft.com/en-us/windows/uwp/graphics-concepts/light-types#point-light
     pub const Point         : LightType = LightType(D3DLIGHT_POINT);
 
-    /// Light is a spotlight source.
+    /// Light is a [spotlight] source.
     /// This light is like a point light, except that the illumination is limited to a cone.
     /// This light type has a direction and several other parameters that determine the shape of the cone it produces.
     /// For information about these parameters, see the [Light] structure.
+    ///
+    /// [spotlight]:    https://docs.microsoft.com/en-us/windows/uwp/graphics-concepts/light-types#spotlight
     pub const Spot          : LightType = LightType(D3DLIGHT_SPOT);
 
-    /// Light is a directional light source. This is equivalent to using a point light source at an infinite distance.
+    /// Light is a [directional] light source. This is equivalent to using a point light source at an infinite distance.
+    ///
+    /// [directional]:  https://docs.microsoft.com/en-us/windows/uwp/graphics-concepts/light-types#directional-light
     pub const Directional   : LightType = LightType(D3DLIGHT_DIRECTIONAL);
 }
 
