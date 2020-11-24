274301c4da71e3abb5ac31440db10e77b6935227
MaulingMonkey <git@maulingmonkey.com>
2020-11-23 19:29:31 -0800

    Rect:  structify

--- src/structs/rect.rs
+++ src/structs/rect.rs
@@ -14,14 +14,36 @@ use std::fmt::{self, Debug, Formatter};
 ///
 /// Two near-identical, ABI-compatible, but slightly differently named/labeled sets of 4 `LONG`s
 ///
-/// | D3DRECT   | RECT      |
-/// | --------- | --------- |
-/// | x1        | left      |
-/// | y1        | top       |
-/// | x2        | right     |
-/// | y2        | bottom    |
-#[derive(Clone, Copy)]
-#[repr(transparent)] pub struct Rect(D3DRECT);
+/// | [Rect] / [D3DRECT]    | [RECT]    |
+/// | --------------------- | --------- |
+/// | x1                    | left      |
+/// | y1                    | top       |
+/// | x2                    | right     |
+/// | y2                    | bottom    |
+#[derive(Clone, Copy, Default)]
+#[repr(C)] pub struct Rect {
+    /// left
+    pub x1: i32,
+
+    /// top
+    pub y1: i32,
+
+    /// right
+    pub x2: i32,
+
+    /// bottom
+    pub y2: i32,
+}
+
+#[cfg(test)] mod vs_d3drect { use super::*; test_layout! { Rect => unsafe D3DRECT { x1 => x1,   y1 => y1,  x2 => x2,    y2 => y2     } } }
+#[cfg(test)] mod vs_rect    { use super::*; test_layout! { Rect => unsafe    RECT { x1 => left, y1 => top, x2 => right, y2 => bottom } } }
+
+impl Deref    for Rect { fn deref    (&    self) -> &    Self::Target { unsafe { std::mem::transmute(self) } } type Target = D3DRECT; }
+impl DerefMut for Rect { fn deref_mut(&mut self) -> &mut Self::Target { unsafe { std::mem::transmute(self) } } }
+impl From<D3DRECT> for Rect { fn from(value: D3DRECT) -> Self { unsafe { std::mem::transmute(value) } } }
+impl From<Rect> for D3DRECT { fn from(value: Rect   ) -> Self { unsafe { std::mem::transmute(value) } } }
+impl From<RECT> for Rect { fn from(value: RECT) -> Self { unsafe { std::mem::transmute(value) } } }
+impl From<Rect> for RECT { fn from(value: Rect) -> Self { unsafe { std::mem::transmute(value) } } }
 
 impl Rect {
     pub fn from(value: impl Into<Self>) -> Self { value.into() }
@@ -45,81 +67,46 @@ impl Debug for Rect {
     }
 }
 
-impl Deref for Rect {
-    type Target = D3DRECT;
-    fn deref(&self) -> &Self::Target { &self.0 }
-}
-
-impl DerefMut for Rect {
-    fn deref_mut(&mut self) -> &mut Self::Target { &mut self.0 }
-}
-
-impl From<RECT> for Rect {
-    fn from(value: RECT) -> Self { Self(D3DRECT { x1: value.left, y1: value.top, x2: value.right, y2: value.bottom }) }
-}
-
-impl From<D3DRECT> for Rect {
-    fn from(value: D3DRECT) -> Self { Self(value) }
-}
-
-impl From<Rect> for RECT {
-    fn from(value: Rect) -> Self { RECT { left: value.0.x1, top: value.0.y1, right: value.0.x2, bottom: value.0.y2 } }
-}
-
-impl From<Rect> for D3DRECT {
-    fn from(value: Rect) -> Self { value.0 }
-}
-
 impl From<(Range<i32>, Range<i32>)> for Rect {
     fn from(value: (Range<i32>, Range<i32>)) -> Self {
-        Self(D3DRECT {
+        Self {
             x1: value.0.start,
             x2: value.0.end,
             y1: value.1.start,
             y2: value.1.end,
-        })
+        }
     }
 }
 
 impl From<Range<(i32, i32)>> for Rect {
     fn from(value: Range<(i32, i32)>) -> Self {
-        Self(D3DRECT {
+        Self {
             x1: value.start.0,
             x2: value.end.0,
             y1: value.start.1,
             y2: value.end.1,
-        })
+        }
     }
 }
 
 impl From<[Range<i32>; 2]> for Rect {
     fn from(value: [Range<i32>; 2]) -> Self {
-        Self(D3DRECT {
+        Self {
             x1: value[0].start,
             x2: value[0].end,
             y1: value[1].start,
             y2: value[1].end,
-        })
+        }
     }
 }
 
 impl From<Range<[i32; 2]>> for Rect {
     fn from(value: Range<[i32; 2]>) -> Self {
-        Self(D3DRECT {
+        Self {
             x1: value.start[0],
             x2: value.end[0],
             y1: value.start[1],
             y2: value.end[1],
-        })
+        }
     }
-}
-
-#[test] fn layout() {
-    use std::mem::*;
-
-    assert_eq!(size_of::<Rect>(), size_of::<D3DRECT>());
-    assert_eq!(size_of::<Rect>(), size_of::<   RECT>());
-
-    assert_eq!(align_of::<Rect>(), align_of::<D3DRECT>());
-    assert_eq!(align_of::<Rect>(), align_of::<   RECT>());
 }
