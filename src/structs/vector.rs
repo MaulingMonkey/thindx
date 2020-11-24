87442dd770b5a926a56d1e0a9a0072f64c979d9f
MaulingMonkey <git@maulingmonkey.com>
2020-11-23 19:22:56 -0800

    Vector:  structify

--- src/structs/vector.rs
+++ src/structs/vector.rs
@@ -2,7 +2,6 @@ use crate::*;
 
 use winapi::shared::d3d9types::*;
 
-use std::fmt::{self, Debug, Formatter};
 use std::ops::{Deref, DerefMut};
 
 
@@ -11,42 +10,16 @@ use std::ops::{Deref, DerefMut};
 /// D3DVECTOR
 ///
 /// Defines a 3-component [f32] vector.
-#[derive(Clone, Copy)]
-#[repr(transparent)] pub struct Vector(D3DVECTOR);
-
-impl Default for Vector {
-    fn default() -> Self {
-        Self(D3DVECTOR {
-            x: 0.0,
-            y: 0.0,
-            z: 0.0
-        })
-    }
+#[derive(Clone, Copy, Debug, Default)]
+#[repr(C)] pub struct Vector {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
 }
 
-impl Deref for Vector {
-    type Target = D3DVECTOR;
-    fn deref(&self) -> &Self::Target { &self.0 }
-}
+impl Deref    for Vector { fn deref    (&    self) -> &    Self::Target { unsafe { std::mem::transmute(self) } } type Target = D3DVECTOR; }
+impl DerefMut for Vector { fn deref_mut(&mut self) -> &mut Self::Target { unsafe { std::mem::transmute(self) } } }
+impl From<D3DVECTOR> for Vector { fn from(value: D3DVECTOR) -> Self { unsafe { std::mem::transmute(value) } } }
+impl From<Vector> for D3DVECTOR { fn from(value: Vector   ) -> Self { unsafe { std::mem::transmute(value) } } }
 
-impl DerefMut for Vector {
-    fn deref_mut(&mut self) -> &mut Self::Target { &mut self.0 }
-}
-
-impl Debug for Vector {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        f.debug_struct("Vector")
-            .field("x", &self.0.x)
-            .field("y", &self.0.y)
-            .field("z", &self.0.z)
-            .finish()
-    }
-}
-
-impl From<D3DVECTOR> for Vector {
-    fn from(value: D3DVECTOR) -> Self { Self(value) }
-}
-
-impl From<Vector> for D3DVECTOR {
-    fn from(value: Vector) -> Self { value.0 }
-}
+test_layout! { Vector => unsafe D3DVECTOR { x => x, y => y, z => z } }
