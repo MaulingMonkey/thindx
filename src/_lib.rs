6593a3f0b8c392c491d39de1226fd126a8e6246a
MaulingMonkey <git@maulingmonkey.com>
2020-11-25 05:16:26 -0800

    extract macros to mod

--- src/_lib.rs
+++ src/_lib.rs
@@ -181,34 +181,8 @@
 //! |                       | Default: disabled     |
 //! | `impl-from-unchecked` | Implement [From]\<[D3DFORMAT]\> for [Format] and similar traits.<br>While these should generally be sound, and may ease porting (e.g. they'll allow using `D3DFMT_UNKNOWN` instead of [Format::UNKNOWN]), they'll also ease authoring bugs (e.g. they'll allow using `9001` for an [Into]\<[Format]\> parameter as well.)
 
-#[cfg(not(test))] macro_rules! test_layout { ( $($tt:tt)* ) => {} }
-
-#[cfg(test)] macro_rules! test_layout {
-    (
-        $thin:ty => unsafe $d3d:ty {
-            $( $thin_field:ident => $d3d_field:ident ),*
-            $(,)?
-        }
-    ) => {
-        #[test] fn layout() {
-            use std::mem::*;
-            let thin = <$thin>::default();
-            let d3d  = unsafe { zeroed::<$d3d>() };
-            assert_eq!( size_of_val(&thin),  size_of_val(&d3d),  "size_of {} != {}", stringify!($thin), stringify!($d3d));
-            assert_eq!(align_of_val(&thin), align_of_val(&d3d), "align_of {} != {}", stringify!($thin), stringify!($d3d));
-            $(
-                assert_eq!(size_of_val(&thin.$thin_field),      size_of_val(&d3d.$d3d_field),       "size_of {}::{} != {}::{}", stringify!($thin), stringify!($thin_field), stringify!($d3d), stringify!($d3d_field));
-                assert_eq!(offset_of(&thin, &thin.$thin_field), offset_of(&d3d, &d3d.$d3d_field), "offset_of {}::{} != {}::{}", stringify!($thin), stringify!($thin_field), stringify!($d3d), stringify!($d3d_field));
-            )*
-        }
-    };
-}
-
-#[cfg(test)] fn offset_of<S, F>(s: &S, f: &F) -> usize {
-    let s : *const S = s;
-    let f : *const F = f;
-    (f as usize) - (s as usize)
-}
+#[macro_use] mod macros;
+#[allow(unused_imports)] use macros::*;
 
 use mcom::Rc;
 
