b40da5ca12be6c44347d1cb8d4c1f504782bdac8
MaulingMonkey <git@maulingmonkey.com>
2020-11-27 19:51:11 -0800

    FunctionLinkingGraph methods (first pass)
    I should add a { ErrorKind, ReadOnlyBlob } struct for general errors

--- src/interfaces/d3d11/function_linking_graph.rs
+++ src/interfaces/d3d11/function_linking_graph.rs
@@ -1,4 +1,10 @@
-#[allow(unused_imports)] use crate::*;
+use crate::*;
+use crate::d3d11::*;
+
+use winapi::um::d3d11shader::*;
+
+use std::convert::TryInto;
+use std::ptr::*;
 
 
 
@@ -8,7 +14,198 @@
 /// A function-linking-graph interface is used for constructing shaders that consist of a
 /// sequence of precompiled function calls that pass values to each other.
 ///
+/// > **Note:** This interface is part of the HLSL shader linking technology that you can use on all Direct3D 11 platforms
+/// > to create precompiled HLSL functions, package them into libraries, and link them into full shaders at run time.
+///
 /// ### See Also
-/// *   [D3DCompiler::create_function_linking_graph]
+/// * [D3DCompiler::create_function_linking_graph]
 #[derive(Clone)] #[repr(transparent)]
 pub struct FunctionLinkingGraph(pub(crate) mcom::Rc<winapi::um::d3d11shader::ID3D11FunctionLinkingGraph>);
+
+impl FunctionLinkingGraph {
+    /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/d3d11shader/nf-d3d11shader-id3d11functionlinkinggraph-callfunction)\]
+    /// ID3D11FunctionLinkingGraph::CallFunction
+    ///
+    /// Creates a call-function linking node to use in the function-linking-graph.
+    ///
+    /// ### Arguments
+    /// *   `module_instance_namespace`         - The optional namespace for the function, or `None` if no namespace is needed.
+    /// *   `module_with_function_prototype`    - A [Module] interface for the library module that contains the function prototype.
+    /// *   `function_name`                     - The name of the function.
+    ///
+    /// ### Example
+    /// ```rust
+    /// # use thin3dcompiler::*; use d3d11::*;
+    /// # let compiler = D3DCompiler::new(47).unwrap();
+    /// # let flg : FunctionLinkingGraph = compiler.create_function_linking_graph(()).unwrap();
+    /// // TODO
+    /// // flg.call_function().unwrap();
+    /// ```
+    pub fn call_function<'s>(
+        &self,
+        module_instance_namespace:      impl Into<Option<&'s str>>,
+        module_with_function_prototype: &Module,
+        function_name:                  &'s str,
+    ) -> Result<LinkingNode, ErrorKind> {
+        let ns      = module_instance_namespace.into().map(|s| s.bytes().chain(Some(0)).collect::<Vec<_>>());
+        let name    = function_name.bytes().chain(Some(0)).collect::<Vec<_>>();
+        let module  = module_with_function_prototype.as_raw();
+
+        let ns = ns.as_ref().map_or(null(), |s| s.as_ptr().cast());
+        let name = name.as_ptr().cast();
+
+        let mut node = null_mut();
+        let hr = unsafe { self.0.CallFunction(ns, module, name, &mut node) };
+        ErrorKind::check(hr)?;
+        Ok(unsafe { LinkingNode::from_raw(node) })
+    }
+
+    /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/d3d11shader/nf-d3d11shader-id3d11functionlinkinggraph-createmoduleinstance)\]
+    /// ID3D11FunctionLinkingGraph::CreateModuleInstance
+    ///
+    /// Initializes a shader module from the function-linking-graph object.
+    ///
+    /// ### Example
+    /// ```rust
+    /// # use thin3dcompiler::*; use d3d11::*;
+    /// # let compiler = D3DCompiler::new(47).unwrap();
+    /// let flg : FunctionLinkingGraph = compiler.create_function_linking_graph(()).unwrap();
+    /// assert_eq!(Some(E::FAIL), flg.create_module_instance().err()); // Nothing created yet
+    /// ```
+    pub fn create_module_instance(&self) -> Result<(ModuleInstance, Option<ReadOnlyBlob>), ErrorKind> {
+        // TODO: named tuple return?  better error type that can carry the blob?
+        let mut module = null_mut();
+        let mut errors = null_mut();
+        let hr = unsafe { self.0.CreateModuleInstance(&mut module, &mut errors) };
+        ErrorKind::check(hr)?;
+        let module = unsafe { ModuleInstance::from_raw(module) };
+        let errors = unsafe { ReadOnlyBlob::from_raw_opt(errors) };
+        Ok((module, errors))
+    }
+
+    /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/d3d11shader/nf-d3d11shader-id3d11functionlinkinggraph-generatehlsl)\]
+    /// ID3D11FunctionLinkingGraph::GenerateHlsl
+    ///
+    /// Generates Microsoft High Level Shader Language (HLSL) shader code that represents the function-linking-graph.
+    ///
+    /// ### Arguments
+    /// *   `flags`             - Reserved, initialize with `()`.
+    ///
+    /// ### Example
+    /// ```rust
+    /// # use thin3dcompiler::*; use d3d11::*;
+    /// # let compiler = D3DCompiler::new(47).unwrap();
+    /// # let flg : FunctionLinkingGraph = compiler.create_function_linking_graph(()).unwrap();
+    /// let hlsl = flg.generate_hlsl(()).unwrap();
+    /// ```
+    pub fn generate_hlsl(&self, flags: ()) -> Result<ReadOnlyBlob, ErrorKind> {
+        let _ = flags; let flags = 0;
+
+        let mut blob = null_mut();
+        let hr = unsafe { self.0.GenerateHlsl(flags, &mut blob) };
+        ErrorKind::check(hr)?;
+        Ok(unsafe { ReadOnlyBlob::from_raw(blob) })
+    }
+
+    /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/d3d11shader/nf-d3d11shader-id3d11functionlinkinggraph-getlasterror)\]
+    /// ID3D11FunctionLinkingGraph::GetLastError
+    ///
+    /// Gets the error from the last function call of the function-linking-graph.
+    ///
+    /// ### Example
+    /// ```rust
+    /// # use thin3dcompiler::*; use d3d11::*;
+    /// # let compiler = D3DCompiler::new(47).unwrap();
+    /// # let flg : FunctionLinkingGraph = compiler.create_function_linking_graph(()).unwrap();
+    /// // TODO
+    /// // flg.get_last_error().unwrap();
+    /// ```
+    pub fn get_last_error(&self) -> Result<ReadOnlyBlob, ErrorKind> {
+        let mut errors = null_mut();
+        let hr = unsafe { self.0.GetLastError(&mut errors) };
+        ErrorKind::check(hr)?;
+        Ok(unsafe { ReadOnlyBlob::from_raw(errors) })
+    }
+
+    /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/d3d11shader/nf-d3d11shader-id3d11functionlinkinggraph-passvalue)\]
+    /// ID3D11FunctionLinkingGraph::PassValue
+    ///
+    /// Passes a value from a source linking node to a destination linking node.
+    ///
+    /// ### Example
+    /// ```rust
+    /// # use thin3dcompiler::*; use d3d11::*;
+    /// # let compiler = D3DCompiler::new(47).unwrap();
+    /// # let flg : FunctionLinkingGraph = compiler.create_function_linking_graph(()).unwrap();
+    /// // TODO
+    /// // flg.pass_value().unwrap();
+    /// ```
+    pub fn pass_value(&self, src_node: &LinkingNode, src_parameter_index: i32, dst_node: &LinkingNode, dst_parameter_index: i32) -> Result<(), ErrorKind> {
+        let hr = unsafe { self.0.PassValue(src_node.as_raw(), src_parameter_index, dst_node.as_raw(), dst_parameter_index) };
+        ErrorKind::check(hr)
+    }
+
+    /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/d3d11shader/nf-d3d11shader-id3d11functionlinkinggraph-passvaluewithswizzle)\]
+    /// ID3D11FunctionLinkingGraph::PassValueWithSwizzle
+    ///
+    /// Passes a value with swizzle from a source linking node to a destination linking node.
+    ///
+    /// ### Example
+    /// ```rust
+    /// # use thin3dcompiler::*; use d3d11::*;
+    /// # let compiler = D3DCompiler::new(47).unwrap();
+    /// # let flg : FunctionLinkingGraph = compiler.create_function_linking_graph(()).unwrap();
+    /// // TODO
+    /// // flg.pass_value_with_swizzle().unwrap();
+    /// ```
+    pub fn pass_value_with_swizzle(&self, src_node: &LinkingNode, src_parameter_index: i32, src_swizzle: &str, dst_node: &LinkingNode, dst_parameter_index: i32, dst_swizzle: &str) -> Result<(), ErrorKind> {
+        let src_swizzle = src_swizzle.bytes().chain(Some(0)).collect::<Vec<_>>();
+        let dst_swizzle = dst_swizzle.bytes().chain(Some(0)).collect::<Vec<_>>();
+        let hr = unsafe { self.0.PassValueWithSwizzle(src_node.as_raw(), src_parameter_index, src_swizzle.as_ptr().cast(), dst_node.as_raw(), dst_parameter_index, dst_swizzle.as_ptr().cast()) };
+        ErrorKind::check(hr)
+    }
+
+    /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/d3d11shader/nf-d3d11shader-id3d11functionlinkinggraph-setinputsignature)\]
+    /// ID3D11FunctionLinkingGraph::SetInputSignature
+    ///
+    /// Sets the input signature of the function-linking-graph.
+    ///
+    /// ### Example
+    /// ```rust
+    /// # use thin3dcompiler::*; use d3d11::*;
+    /// # let compiler = D3DCompiler::new(47).unwrap();
+    /// # let flg : FunctionLinkingGraph = compiler.create_function_linking_graph(()).unwrap();
+    /// // TODO
+    /// // flg.set_input_signature().unwrap();
+    /// ```
+    pub unsafe fn set_input_signature_raw(&self, input_parameters: &[D3D11_PARAMETER_DESC]) -> Result<LinkingNode, ErrorKind> {
+        let n = input_parameters.len().try_into().map_err(|_| ErrorKind::SLICE_TOO_LARGE)?;
+
+        let mut node = null_mut();
+        let hr = self.0.SetInputSignature(input_parameters.as_ptr(), n, &mut node);
+        ErrorKind::check(hr)?;
+        Ok(LinkingNode::from_raw(node))
+    }
+
+    /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/d3d11shader/nf-d3d11shader-id3d11functionlinkinggraph-setoutputsignature)\]
+    /// ID3D11FunctionLinkingGraph::SetOutputSignature
+    ///
+    /// Sets the output signature of the function-linking-graph.
+    ///
+    /// ### Example
+    /// ```rust
+    /// # use thin3dcompiler::*; use d3d11::*;
+    /// # let compiler = D3DCompiler::new(47).unwrap();
+    /// # let flg : FunctionLinkingGraph = compiler.create_function_linking_graph(()).unwrap();
+    /// // TODO
+    /// // flg.set_output_signature().unwrap();
+    /// ```
+    pub unsafe fn set_output_signature_raw(&self, output_parameters: &[D3D11_PARAMETER_DESC]) -> Result<LinkingNode, ErrorKind> {
+        let n = output_parameters.len().try_into().map_err(|_| ErrorKind::SLICE_TOO_LARGE)?;
+
+        let mut node = null_mut();
+        let hr = self.0.SetOutputSignature(output_parameters.as_ptr(), n, &mut node);
+        ErrorKind::check(hr)?;
+        Ok(LinkingNode::from_raw(node))
+    }
+}
