183d779112072b12edd72925f1b6b7eac5001d75
MaulingMonkey <git@maulingmonkey.com>
2020-11-28 03:09:20 -0800

    document Raw

--- src/traits/raw.rs
+++ src/traits/raw.rs
@@ -1,10 +1,30 @@
 use std::marker::Sized;
+#[allow(unused_imports)] use std::ptr::*;
 
+
+
+/// Allow conversion to/from raw (winapi) pointer types.
+///
+/// ### Safety
+///
+/// Lots of code depends on implicit invariants of this trait for soundness:
+///
+/// *   [Raw::into_raw] and [Raw::as_raw] have sane implementations.
+/// *   If [Raw::Raw] is a [winapi::Interface], it too must be "sane" (correct guid, sound implementation of the COM interface in question, etc.)
+/// *   Probably more I'm not thinking of
 pub unsafe trait Raw : Sized {
+    /// The raw underlying winapi type
     type Raw : Sized;
 
+    /// Take ownership from a raw winapi type, panicing if `raw` is [null_mut].
     unsafe fn from_raw(raw: *mut Self::Raw) -> Self;
+
+    /// Take ownership from a raw winapi type, returning [None] if `raw` is [null_mut].
     unsafe fn from_raw_opt(raw: *mut Self::Raw) -> Option<Self>;
+
+    /// Give up / leak ownership into a raw winapi pointer type.
     fn into_raw(self) -> *mut Self::Raw;
+
+    /// Allow access as a raw winapi pointer type.
     fn as_raw(&self) -> *mut Self::Raw;
 }
