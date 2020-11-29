671fd5a9fe413c56d8e405b141b1944d371c5594
MaulingMonkey <git@maulingmonkey.com>
2020-11-28 23:36:21 -0800

    ID3D11ShaderReflectionVariable methods

--- src/interfaces/d3d11/shader_reflection_variable.rs
+++ src/interfaces/d3d11/shader_reflection_variable.rs
@@ -30,4 +30,65 @@ impl<'r> ShaderReflectionVariable<'r> {
     }
 }
 
-// TODO: methods
+impl<'r> ShaderReflectionVariable<'r> {
+    /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/d3d11shader/nf-d3d11shader-id3d11shaderreflectionvariable-getbuffer)\]
+    /// ID3D11ShaderReflectionVariable::GetBuffer
+    ///
+    /// This method returns the buffer of the current ID3D11ShaderReflectionVariable.
+    ///
+    /// ### Example
+    /// ```rust
+    /// # use thin3dcompiler::*;
+    /// // TODO
+    /// ```
+    pub fn get_buffer(&self) -> Option<ShaderReflectionConstantBuffer> {
+        let ptr = unsafe { self.ptr.as_ref().GetBuffer() };
+        unsafe { ShaderReflectionConstantBuffer::from_raw(self.phantom, ptr) }
+    }
+
+    /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/d3d11shader/nf-d3d11shader-id3d11shaderreflectionvariable-getdesc)\]
+    /// ID3D11ShaderReflectionVariable::GetDesc
+    ///
+    /// Get a shader-variable description.
+    ///
+    /// ### Example
+    /// ```rust
+    /// # use thin3dcompiler::*;
+    /// // TODO
+    /// ```
+    pub fn get_desc_raw(&self) -> Result<D3D11_SHADER_VARIABLE_DESC, Error> {
+        let mut desc = unsafe { std::mem::zeroed::<D3D11_SHADER_VARIABLE_DESC>() };
+        let hr = unsafe { self.ptr.as_ref().GetDesc(&mut desc) };
+        Error::check("ID3D11ShaderReflectionVariable::GetDesc", hr)?;
+        Ok(desc)
+    }
+
+    /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/d3d11shader/nf-d3d11shader-id3d11shaderreflectionvariable-getinterfaceslot)\]
+    /// ID3D11ShaderReflectionVariable::GetInterfaceSlot
+    ///
+    /// Gets the corresponding interface slot for a variable that represents an interface pointer.
+    ///
+    /// ### Example
+    /// ```rust
+    /// # use thin3dcompiler::*;
+    /// // TODO
+    /// ```
+    pub fn get_interface_slot(&self, array_index: u32) -> u32 {
+        unsafe { self.ptr.as_ref().GetInterfaceSlot(array_index) }
+    }
+
+    /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/d3d11shader/nf-d3d11shader-id3d11shaderreflectionvariable-gettype)\]
+    /// ID3D11ShaderReflectionVariable::GetType
+    ///
+    /// Get a shader-variable type.
+    ///
+    /// ### Example
+    /// ```rust
+    /// # use thin3dcompiler::*;
+    /// // TODO
+    /// ```
+    pub fn get_type(&self) -> Option<ShaderReflectionType> {
+        let ptr = unsafe { self.ptr.as_ref().GetType() };
+        unsafe { ShaderReflectionType::from_raw(self.phantom, ptr) }
+    }
+}
