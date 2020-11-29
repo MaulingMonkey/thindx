887cfe3f4309b046a63b9b86c25c5ef04ce006be
MaulingMonkey <git@maulingmonkey.com>
2020-11-29 08:36:44 -0800

    TessellatorDomain values

--- src/enumerations/tessellator_domain.rs
+++ src/enumerations/tessellator_domain.rs
@@ -9,16 +9,22 @@ use winapi::um::d3dcommon::*;
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[repr(transparent)] pub struct TessellatorDomain(D3D_TESSELLATOR_DOMAIN);
 
-enumish! {
-    TessellatorDomain => D3D_TESSELLATOR_DOMAIN;
-}
+// Note: D3D11_* aliases D3D_* despite poor docs kinda implying otherwise (see d3dcommon.h)
+
+enumish! { TessellatorDomain => D3D_TESSELLATOR_DOMAIN; Undefined, IsoLine, Tri, Quad }
 
 #[allow(non_upper_case_globals)] impl TessellatorDomain { // These are enum-like
-    // TODO
+    pub const Undefined : TessellatorDomain = TessellatorDomain(D3D_TESSELLATOR_DOMAIN_UNDEFINED);
+    pub const IsoLine   : TessellatorDomain = TessellatorDomain(D3D_TESSELLATOR_DOMAIN_ISOLINE);
+    pub const Tri       : TessellatorDomain = TessellatorDomain(D3D_TESSELLATOR_DOMAIN_TRI);
+    pub const Quad      : TessellatorDomain = TessellatorDomain(D3D_TESSELLATOR_DOMAIN_QUAD);
 }
 
 #[doc(hidden)] impl TessellatorDomain { // Ctrl+C Ctrl+V support
-    // TODO
+    pub const UNDEFINED : TessellatorDomain = TessellatorDomain(D3D_TESSELLATOR_DOMAIN_UNDEFINED);
+    pub const ISOLINE   : TessellatorDomain = TessellatorDomain(D3D_TESSELLATOR_DOMAIN_ISOLINE);
+    pub const TRI       : TessellatorDomain = TessellatorDomain(D3D_TESSELLATOR_DOMAIN_TRI);
+    pub const QUAD      : TessellatorDomain = TessellatorDomain(D3D_TESSELLATOR_DOMAIN_QUAD);
 }
 
 impl Default for TessellatorDomain {
