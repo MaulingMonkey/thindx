02fe4f09180f8cd0d4cf8e11ab96ea8dfb7d2033
MaulingMonkey <git@maulingmonkey.com>
2020-11-29 08:39:16 -0800

    TessellatorPartitioning values

--- src/enumerations/tessellator_partitioning.rs
+++ src/enumerations/tessellator_partitioning.rs
@@ -9,16 +9,22 @@ use winapi::um::d3dcommon::*;
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[repr(transparent)] pub struct TessellatorPartitioning(D3D_TESSELLATOR_PARTITIONING);
 
-enumish! {
-    TessellatorPartitioning => D3D_TESSELLATOR_PARTITIONING;
-}
+enumish! { TessellatorPartitioning => D3D_TESSELLATOR_PARTITIONING; Undefined, Integer, Pow2, FractionalOdd, FractionalEven }
 
 #[allow(non_upper_case_globals)] impl TessellatorPartitioning { // These are enum-like
-    // TODO
+    pub const Undefined         : TessellatorPartitioning = TessellatorPartitioning(D3D_TESSELLATOR_PARTITIONING_UNDEFINED);
+    pub const Integer           : TessellatorPartitioning = TessellatorPartitioning(D3D_TESSELLATOR_PARTITIONING_INTEGER);
+    pub const Pow2              : TessellatorPartitioning = TessellatorPartitioning(D3D_TESSELLATOR_PARTITIONING_POW2);
+    pub const FractionalOdd     : TessellatorPartitioning = TessellatorPartitioning(D3D_TESSELLATOR_PARTITIONING_FRACTIONAL_ODD);
+    pub const FractionalEven    : TessellatorPartitioning = TessellatorPartitioning(D3D_TESSELLATOR_PARTITIONING_FRACTIONAL_EVEN);
 }
 
 #[doc(hidden)] impl TessellatorPartitioning { // Ctrl+C Ctrl+V support
-    // TODO
+    pub const UNDEFINED         : TessellatorPartitioning = TessellatorPartitioning(D3D_TESSELLATOR_PARTITIONING_UNDEFINED);
+    pub const INTEGER           : TessellatorPartitioning = TessellatorPartitioning(D3D_TESSELLATOR_PARTITIONING_INTEGER);
+    pub const POW2              : TessellatorPartitioning = TessellatorPartitioning(D3D_TESSELLATOR_PARTITIONING_POW2);
+    pub const FRACTIONAL_ODD    : TessellatorPartitioning = TessellatorPartitioning(D3D_TESSELLATOR_PARTITIONING_FRACTIONAL_ODD);
+    pub const FRACTIONAL_EVEN   : TessellatorPartitioning = TessellatorPartitioning(D3D_TESSELLATOR_PARTITIONING_FRACTIONAL_EVEN);
 }
 
 impl Default for TessellatorPartitioning {
