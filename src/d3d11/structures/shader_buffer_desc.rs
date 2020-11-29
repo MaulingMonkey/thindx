488f2c665c432ca58e051d1b5e2d01330d962929
MaulingMonkey <git@maulingmonkey.com>
2020-11-29 06:34:29 -0800

    D3D11_SHADER_BUFFER_DESC fix doc link

--- src/d3d11/structures/shader_buffer_desc.rs
+++ src/d3d11/structures/shader_buffer_desc.rs
@@ -5,7 +5,7 @@ use winapi::um::d3d11shader::*;
 
 
 
-/// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/d3d11shader/ns-d3d11shader-d3d11_library_desc)\]
+/// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/d3d11shader/ns-d3d11shader-d3d11_shader_buffer_desc)\]
 /// D3D11_SHADER_BUFFER_DESC
 ///
 /// &amp;\[[u8]\] equivalent that's ABI-compatible with some D3D APIs
