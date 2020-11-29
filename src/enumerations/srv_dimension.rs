687fdbbf0dbec962f00ab3c523a51490d98251af
MaulingMonkey <git@maulingmonkey.com>
2020-11-29 08:34:52 -0800

    SrvDimension values

--- src/enumerations/srv_dimension.rs
+++ src/enumerations/srv_dimension.rs
@@ -9,16 +9,44 @@ use winapi::um::d3dcommon::*;
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[repr(transparent)] pub struct SrvDimension(D3D_SRV_DIMENSION);
 
+// Note: D3D10_*   aliases D3D_* despite poor docs kinda implying otherwise (see d3dcommon.h)
+// Note: D3D10_1_* aliases D3D_* despite poor docs kinda implying otherwise (see d3dcommon.h)
+// Note: D3D11_*   aliases D3D_* despite poor docs kinda implying otherwise (see d3dcommon.h)
+
 enumish! {
     SrvDimension => D3D_SRV_DIMENSION;
+    Unknown, Buffer, Texture1D, Texture1DArray, Texture2D, Texture2DArray, Texture2DMS, Texture2DMSArray, Texture3D,
+    TextureCube, TextureCubeArray, BufferEx,
 }
 
 #[allow(non_upper_case_globals)] impl SrvDimension { // These are enum-like
-    // TODO
+    pub const Unknown           : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_UNKNOWN);
+    pub const Buffer            : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_BUFFER);
+    pub const Texture1D         : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_TEXTURE1D);
+    pub const Texture1DArray    : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_TEXTURE1DARRAY);
+    pub const Texture2D         : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_TEXTURE2D);
+    pub const Texture2DArray    : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_TEXTURE2DARRAY);
+    pub const Texture2DMS       : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_TEXTURE2DMS);
+    pub const Texture2DMSArray  : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_TEXTURE2DMSARRAY);
+    pub const Texture3D         : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_TEXTURE3D);
+    pub const TextureCube       : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_TEXTURECUBE);
+    pub const TextureCubeArray  : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_TEXTURECUBEARRAY);
+    pub const BufferEx          : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_BUFFEREX);
 }
 
 #[doc(hidden)] impl SrvDimension { // Ctrl+C Ctrl+V support
-    // TODO
+    pub const UNKNOWN           : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_UNKNOWN);
+    pub const BUFFER            : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_BUFFER);
+    pub const TEXTURE1D         : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_TEXTURE1D);
+    pub const TEXTURE1DARRAY    : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_TEXTURE1DARRAY);
+    pub const TEXTURE2D         : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_TEXTURE2D);
+    pub const TEXTURE2DARRAY    : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_TEXTURE2DARRAY);
+    pub const TEXTURE2DMS       : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_TEXTURE2DMS);
+    pub const TEXTURE2DMSARRAY  : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_TEXTURE2DMSARRAY);
+    pub const TEXTURE3D         : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_TEXTURE3D);
+    pub const TEXTURECUBE       : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_TEXTURECUBE);
+    pub const TEXTURECUBEARRAY  : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_TEXTURECUBEARRAY);
+    pub const BUFFEREX          : SrvDimension = SrvDimension(D3D_SRV_DIMENSION_BUFFEREX);
 }
 
 impl Default for SrvDimension {
