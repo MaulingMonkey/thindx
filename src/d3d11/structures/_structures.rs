05c4bffd69362e0caece7e355be2e40eeef50bc6
MaulingMonkey <git@maulingmonkey.com>
2020-11-29 06:36:36 -0800

    D3D11_SHADER_TYPE_DESC

--- src/d3d11/structures/_structures.rs
+++ src/d3d11/structures/_structures.rs
@@ -6,7 +6,7 @@ mod parameter_desc;                     pub use parameter_desc::*;
 mod shader_buffer_desc;                 pub use shader_buffer_desc::*;
 mod shader_desc;                        pub use shader_desc::*;
 mod shader_input_bind_desc;             pub use shader_input_bind_desc::*;
+mod shader_type_desc;                   pub use shader_type_desc::*;
 
-// D3D11_SHADER_TYPE_DESC
 // D3D11_SHADER_VARIABLE_DESC
 // D3D11_SIGNATURE_PARAMETER_DESC
---
+++ src/d3d11/structures/shader_type_desc.rs
@@ -0,0 +1,33 @@
+use crate::*;
+use crate::d3d11::*;
+
+use winapi::um::d3d11shader::*;
+
+
+
+/// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/d3d11shader/ns-d3d11shader-d3d11_shader_type_desc)\]
+/// D3D11_SHADER_TYPE_DESC
+///
+/// &amp;\[[u8]\] equivalent that's ABI-compatible with some D3D APIs
+#[derive(Clone, Copy, Default)]
+#[repr(C)] pub struct ShaderTypeDesc<'s> {
+    class:      ShaderVariableClass,
+    r#type:     ShaderVariableType,
+    rows:       u32,
+    columns:    u32,
+    elements:   u32,
+    members:    u32,
+    offset:     u32,
+    name:       Option<CStrPtr<'s>>,
+}
+
+test_layout! { ShaderTypeDesc => unsafe D3D11_SHADER_TYPE_DESC {
+    class       => Class,
+    r#type      => Type,
+    rows        => Rows,
+    columns     => Columns,
+    elements    => Elements,
+    members     => Members,
+    offset      => Offset,
+    name        => Name,
+}}
