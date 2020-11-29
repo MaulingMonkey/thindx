1d125e41bdad365095b3b14b08fda70f39518491
MaulingMonkey <git@maulingmonkey.com>
2020-11-29 00:00:21 -0800

    rename dll -> d3dcompiler

--- src/_lib.rs
+++ src/_lib.rs
@@ -146,7 +146,7 @@
 
 
 
-#[path="dll/_dll.rs"]                   mod dll;            pub use dll::*;
+#[path="d3dcompiler/_d3dcompiler.rs"]   mod d3dcompiler;    pub use d3dcompiler::*;
 #[path="enumerations/_enumerations.rs"] mod enumerations;   pub use enumerations::*;
 #[path="flags/_flags.rs"]               mod flags;          pub use flags::*;
 #[path="interfaces/_interfaces.rs"]     mod interfaces;     pub use interfaces::*;
