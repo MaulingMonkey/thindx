e1686175a003fc7c352e205a95643358d41ec95a
MaulingMonkey <git@maulingmonkey.com>
2020-12-02 08:41:50 -0800

    FunctionDesc example

--- thindx/src/headers/d3d11shader.h/structures/function_desc.rs
+++ thindx/src/headers/d3d11shader.h/structures/function_desc.rs
@@ -6,6 +6,48 @@ use winapi::um::d3d11shader::*;
 
 /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/d3d11shader/ns-d3d11shader-d3d11_function_desc)\]
 /// D3D11_FUNCTION_DESC
+///
+/// ### See Also
+/// *   [d3d11::FunctionReflection::get_desc]
+///
+/// ### Example
+/// ```text
+/// FunctionDesc {
+///     version: 4293918800,
+///     creator: Some("Microsoft (R) HLSL Shader Compiler 10.1"),
+///     flags: Compile::{Debug|NoPreshader},
+///     constant_buffers: 1,
+///     bound_resources: 1,
+///     instruction_count: 2,
+///     temp_register_count: 0,
+///     temp_array_count: 0,
+///     def_count: 0,
+///     dcl_count: 2,
+///     texture_normal_nstructions: 0,
+///     texture_load_instructions: 0,
+///     texture_comp_instructions: 0,
+///     texture_bias_instructions: 0,
+///     texture_gradient_nstructions: 0,
+///     float_instruction_count: 1,
+///     int_instruction_count: 0,
+///     uint_instruction_count: 0,
+///     static_flow_control_count: 1,
+///     dynamic_flow_control_count: 0,
+///     macro_instruction_count: 0,
+///     array_instruction_count: 0,
+///     mov_instruction_count: 0,
+///     movc_instruction_count: 0,
+///     conversion_instruction_count: 0,
+///     bitwise_instruction_count: 0,
+///     min_feature_level: FeatureLevel::_11_0,
+///     required_feature_flags: ShaderRequires::None,
+///     name: Some("scale4"),
+///     function_parameter_count: 1,
+///     has_return: true,
+///     has_10_level_9_vertex_shader: false,
+///     has_10_level_9_pixel_shader: false,
+/// }
+/// ```
 #[derive(Clone, Copy, Debug, Default)]
 #[repr(C)] pub struct FunctionDesc<'s> {
     pub version:                        u32,
