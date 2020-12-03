a917a26a048dcc00be44a97570f409f5245261ab
MaulingMonkey <git@maulingmonkey.com>
2020-12-03 15:32:01 -0800

    more ReadOnlyBlob traits

--- thindx/src/headers/d3dcommon.h/interfaces/blob.rs
+++ thindx/src/headers/d3dcommon.h/interfaces/blob.rs
@@ -2,6 +2,11 @@ use crate::*;
 
 use winapi::um::d3dcommon::ID3DBlob;
 
+use std::borrow::Borrow;
+use std::fmt::{self, Debug, Formatter};
+use std::ops::Index;
+use std::slice::SliceIndex;
+
 
 
 /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/previous-versions/windows/desktop/legacy/ff728743(v=vs.85))\]
@@ -37,7 +42,23 @@ impl ReadOnlyBlob {
         let ptr = unsafe { self.0.GetBufferPointer() } as *const u8;
         unsafe { std::slice::from_raw_parts(ptr, size) }
     }
+
+    // as_bytes? bytes? to_bytes? iter?
 }
+
+impl AsRef <[u8]> for ReadOnlyBlob { fn as_ref(&self) -> &[u8] { self.get_buffer() } }
+impl Borrow<[u8]> for ReadOnlyBlob { fn borrow(&self) -> &[u8] { self.get_buffer() } }
+impl Debug        for ReadOnlyBlob { fn fmt(&self, fmt: &mut Formatter) -> fmt::Result { write!(fmt, "ReadOnlyBlob({} bytes)", self.get_buffer_size()) } }
+impl<I> Index<I>  for ReadOnlyBlob where I: SliceIndex<[u8]> { fn index(&self, index: I) -> &Self::Output { self.get_buffer().index(index) } type Output = I::Output; }
+
+// can't impl Deref - conflicting impl with COM nonsense
+
+// Index?
+// IntoIterator?
+// [Partial]Ord/[Partial]Eq? (nah - ambiguous between bytes vs com instances)
+// Hash? (nah - might want to allow Borrow<[u32]>?)
+
+// From?  Would require manual ID3DBlob impls in case d3dcompiler.dll isn't available/loaded?
 
 #[test] fn layout() {
     use std::mem::*;
