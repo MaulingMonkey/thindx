7a1265ba51241ef899dc0c86bf10f60e6d0476b2
MaulingMonkey <git@maulingmonkey.com>
2020-12-04 03:17:52 -0800

    _lib.rs: update intro, stub d3d mod

--- thindx/src/_lib.rs
+++ thindx/src/_lib.rs
@@ -15,104 +15,22 @@
 //!     interoperability with other graphics code, or for accomplishing incremental rewrites.
 //!
 //! ### Is this crate sound?
-//! ❌  Lack of thorough testing coverage means undefined behavior (integer overflows? parser/deserialization bugs?) in
-//! d3dcompiler_##.dll probably leads to a few soundness holes in individual `fn`s.
-//!
-//! ⚠️  The public API shape/design can probably be made safe with few if any changes, through the addition of extra
-//! internal bounds checks and other parameter validation.  The main caveat here is bytecode deserialization - it may
-//! later turn out that I need to change the API to make constructing shader blobs from untrusted sources `unsafe`,
-//! unless I want to write my own bytecode validator from scratch (I don't, way out of project scope!)
-//!
-//! ### Quickstart
-//!
-//! ```rust
-//! use thindx::*;
-//!
-//! let compiler = D3DCompiler::new(47).unwrap(); // Loads d3dcompiler_47.dll
-//!
-//! // Compile a pixel shader to shader model 4.0 bytecode
-//! let result = compiler.compile_from_file(
-//!     r"test\data\basic.hlsl", None, None, "ps_main", "ps_4_0",
-//!     Compile::Debug, CompileEffect::None,
-//! );
-//!
-//! // You want diagnostics?  You got diagnostics!
-//! let pixel_shader : ReadOnlyBlob = match result {
-//!     Ok(CompileResult { shader, errors: None }) => shader,
-//!     Ok(CompileResult { shader, errors: Some(errors) }) => {
-//!         println!("Shader built OK with warnings:");
-//!         println!("{}", String::from_utf8_lossy(errors.get_buffer()));
-//!         shader
-//!     },
-//!     Err(error) => {
-//!         println!("Shader failed to build:");
-//!         println!("{}", error);
-//!         return;
-//!     },
-//! };
-//!
-//! // Get the raw bytes out of the ReadOnlyBlob
-//! let bytes : &[u8] = pixel_shader.get_buffer();
-//! ```
-//!
-//! See [D3DCompiler] for a *lot* more methods you can use!
-//!
-//! ### Factories
-//!
-//! | thindx                                        | docs.microsoft.com    | Desc |
-//! | --------------------------------------------- | --------------------- | ---- |
-//! | [D3DCompiler]                                 | `d3dcompiler_##.dll`  | The main factory for FXC bytecode (shader model 5 and earlier)
-//! | <span class="inaccurate">DXCompiler</span>    | `dxcompiler.dll`      | The main factory for DXC bytecode (shader model 6+)
-//!
-//! ### Enumerations
-//!
-//! | thindx                | docs.microsoft.com    | Desc |
-//! | --------------------- | --------------------- | ---- |
-//! | [BlobPart]            | [D3D_BLOB_PART]       | Identifies a subsection of a bytecode blob
-//! | [ErrorKind]           | HRESULT               | Error kind, used throughout the API
-//!
-//! [D3D_BLOB_PART]:        https://docs.microsoft.com/en-us/windows/win32/api/d3dcompiler/ne-d3dcompiler-d3d_blob_part
-//!
-//! ### Flags
-//!
-//! | thindx                | docs.microsoft.com    | Desc |
-//! | --------------------- | --------------------- | ---- |
-//! | [Compile]             | [D3DCOMPILE_\*]           | Flags controlling how HLSL is compiled to bytecode
-//! | [CompileEffect]       | [D3DCOMPILE_EFFECT_\*]    | Flags controlling how HLSL **effects** shaders are compiled
-//! | [CompileSecdata]      | [D3DCOMPILE_SECDATA_\*]   | Flags controlling how HLSL is compiled to bytecode via [compile2](D3DCompiler::compile2)
-//! | [CompilerStrip]       | [D3DCOMPILER_STRIP_\*]    | Flags controlling what data is stripped by [D3DCompiler::strip_shader]
-//! | [CompressShader]      | [D3D_COMPRESS_SHADER_\*]  | Flags controlling the behavior of [D3DCompiler::compress_shaders]
-//! | [Disasm]              | [D3D_DISASM_\*]           | Flags controlling how [disassemble_region](D3DCompiler::disassemble_region) disassembles shader data
-//! | [GetInstOffsets]      | [D3D_GET_INST_OFFSETS_\*] | Flags controlling [D3DCompiler::get_trace_instruction_offsets]
-//!
-//! [D3DCOMPILE_EFFECT_\*]:     https://docs.microsoft.com/en-us/windows/win32/direct3dhlsl/d3dcompile-effect-constants
-//! [D3DCOMPILE_SECDATA_\*]:    https://docs.microsoft.com/en-us/windows/win32/api/d3dcompiler/nf-d3dcompiler-d3dcompile2#parameters
-//! [D3DCOMPILE_\*]:            https://docs.microsoft.com/en-us/windows/win32/direct3dhlsl/d3dcompile-constants
-//! [D3DCOMPILER_STRIP_\*]:     https://docs.microsoft.com/en-us/windows/win32/api/d3dcompiler/ne-d3dcompiler-d3dcompiler_strip_flags
-//! [D3D_COMPRESS_SHADER_\*]:   https://docs.microsoft.com/en-us/windows/win32/api/d3dcompiler/nf-d3dcompiler-d3dcompressshaders#parameters
-//! [D3D_DISASM_\*]:            https://docs.microsoft.com/en-us/windows/win32/api/d3dcompiler/nf-d3dcompiler-d3ddisassembleregion#parameters
-//! [D3D_GET_INST_OFFSETS_\*]:  https://docs.microsoft.com/en-us/windows/win32/api/d3dcompiler/nf-d3dcompiler-d3dgettraceinstructionoffsets#parameters
-//!
-//! ### Structures
-//!
-//! | thindx                | docs.microsoft.com    | Desc |
-//! | --------------------- | --------------------- | ---- |
-//! | [ShaderData]          | [D3D_SHADER_DATA]     | &amp;\[[u8]\] equivalent that's ABI-compatible with some D3D APIs
-//! | [Error]               | <span style="opacity: 33%">N/A</span>
-//!
-//! [D3D_SHADER_DATA]:          https://docs.microsoft.com/en-us/windows/win32/api/d3dcompiler/ns-d3dcompiler-d3d_shader_data
-//!
-//! ### Traits
-//!
-//! | thindx                | docs.microsoft.com    | Desc |
-//! | --------------------- | --------------------- | ---- |
-//! | [AsID3DInclude]       | [ID3DInclude]         | Conversion trait for resolving `#include`s
-//! | [AsShaderMacros]      | [D3D_SHADER_MACRO]\[\]| Conversion trait for resolving `#define`s
-//! | [Raw]                 | <span style="opacity: 33%">N/A</span> | Conversion to/from raw [winapi] types
-//!
-//! [ID3DInclude]:              https://docs.microsoft.com/en-us/windows/win32/api/d3dcommon/nn-d3dcommon-id3dinclude
-//! [D3D_SHADER_MACRO]:         https://docs.microsoft.com/en-us/windows/win32/api/d3dcommon/ns-d3dcommon-d3d_shader_macro
-
+//! 
+//! Probably not.  I'm exposing a huge legacy C++ API to Rust.  Mistakes will happen.
+//! 
+//! That said, soundness *is* a **very high priority** goal.  `thindx` will add things like extra bounds checks, parameter
+//! validation, extra init, etc. if need be in order to ensure soundness in safe fns whenever possible.  When it's not
+//! possible to validate unsoundness away, the fns in question should be marked `unsafe`.  This crate strives to be sounder
+//! than whatever manual FFI you'd write yourself would be, and that's a **high** bar.
+//! 
+//! But there are some practical limits to this.  If a background driver thread invokes UB if it fails to allocate memory,
+//! without any direct correlation to specific API misuse like a large integer overflowing, that's a bug I can't sanely
+//! mitigate via safe fns.  I mean, theoretically I could write a pure-software clone of the entire DirectX runtime... but no.
+//! 
+//! Additionally, while I'm seeking to validate my APIs via testing, older implementations of the APIs in question may have
+//! more bugs / unchecked parameters / ??? that I'll fail to mitigate due to being unable to trigger them myself.  While I'm
+//! happy to investigate, accept pull requests, expand test coverage, etc. it's worth assuming this crate is unsound on
+//! older versions unless you've tested yourself.
 
 
 #![allow(broken_intra_doc_links)] // temporary
@@ -124,6 +42,12 @@ use thindx_zzz_internal_proc_macros::*;
 pub use abibool::bool32;
 mod abi_cstr; pub use abi_cstr::*;
 
+/// Direct3D related types and APIs used across multiple Direct3D versions.
+pub mod d3d {
+    // todo
+}
+
+/// Direct3D 11 related types and APIs (including shader reflection APIs)
 pub mod d3d11 {
     pub use crate::d3d11shader_h::*;
 }
