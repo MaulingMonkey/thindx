5e14843c34b91e2f7f41c4ad03a383fa875a34ec
MaulingMonkey <git@maulingmonkey.com>
2020-12-14 09:27:09 -0800

    More source code validation
    - Validate existence of safety sections on unsafe items
    - Validate arguments only exist for fns/macros

--- xtask/source.rs
+++ xtask/source.rs
@@ -52,6 +52,9 @@ fn scan_file_doc_comments(path: &Path, text: &str) -> Result<(), ()> {
         current_h3:             Option<H3>,
         errors:                 bool,
         mode:                   Mode,
+        arguments_line_no:      Option<usize>,
+        safety_line_no:         Option<usize>,
+        arguments:              Vec<String>,
     }
 
     impl State {
@@ -78,9 +81,17 @@ fn scan_file_doc_comments(path: &Path, text: &str) -> Result<(), ()> {
                 },
             }
 
-            self.current_comment_is_mod  = None;
-            self.current_h3              = None;
-            self.mode                    = Mode::Default;
+            if !self.arguments.is_empty() {
+                error!(at: path, line: last_comment_line_no, "Expected fn describing `### Arguments`");
+                self.errors = true;
+            }
+
+            self.current_comment_is_mod = None;
+            self.current_h3             = None;
+            self.mode                   = Mode::Default;
+            self.arguments_line_no      = None;
+            self.safety_line_no         = None;
+            self.arguments.clear();
         }
     }
 
@@ -88,8 +99,10 @@ fn scan_file_doc_comments(path: &Path, text: &str) -> Result<(), ()> {
 
     let mut lines = text.lines().enumerate();
 
+    let mut last_line_no = 0;
     while let Some((idx, line)) = lines.next() {
         let no = idx+1;
+        last_line_no = no;
         let trimmed = line.trim();
 
         let comment = if let Some(comment) = trimmed.strip_prefix("//!") {
@@ -102,7 +115,120 @@ fn scan_file_doc_comments(path: &Path, text: &str) -> Result<(), ()> {
             if s.current_comment_is_mod != Some(false) { s.on_comment_end(path, idx); }
             s.current_comment_is_mod = Some(false);
             comment
-        } else if s.current_comment_is_mod.is_some() {
+        } else if trimmed.starts_with("//") {
+            continue // ignore regular comment lines
+        } else if s.current_comment_is_mod == Some(true) {
+            s.on_comment_end(path, idx);
+            continue
+        } else if s.current_comment_is_mod == Some(false) {
+            if trimmed.is_empty() {
+                error!(at: path, line: no, "unexpected blank line after doc comment");
+                s.errors = true;
+                continue;
+            }
+
+            let mut rest = trimmed;
+
+            loop {
+                if let Some(r) = rest.strip_prefix("#[repr(transparent)]") {
+                    rest = r.trim_start();
+                    //s.repr = Some(Transparent);
+                } else if let Some(r) = rest.strip_prefix("#[repr(C)]") {
+                    rest = r.trim_start();
+                    //s.repr = Some(C);
+                } else if let Some(r) = rest.strip_prefix("#[allow(") {
+                    let end = match r.find(")]") {
+                        Some(n) => n,
+                        None => {
+                            error!(at: path, line: no, "#[allow(...)] expected to be a single line");
+                            s.errors = true;
+                            continue;
+                        },
+                    };
+                    rest = r[end+2..].trim_start();
+                } else if let Some(r) = rest.strip_prefix("#[derive(") {
+                    let end = match r.find(")]") {
+                        Some(n) => n,
+                        None => {
+                            error!(at: path, line: no, "#[derive(...)] expected to be a single line");
+                            s.errors = true;
+                            continue;
+                        },
+                    };
+                    rest = r[end+2..].trim_start();
+                } else if let Some(r) = rest.strip_prefix("#[requires(") {
+                    let end = match r.find(")]") {
+                        Some(n) => n,
+                        None => {
+                            error!(at: path, line: no, "#[requires(...)] expected to be a single line");
+                            s.errors = true;
+                            continue;
+                        },
+                    };
+                    rest = r[end+2..].trim_start();
+                } else if rest.starts_with("//") {
+                    rest = "";
+                } else {
+                    break;
+                }
+            }
+
+            if rest.is_empty() {
+                continue; // not an error
+            }
+
+            let (_vis, rest) = if let Some(rest) = rest.strip_prefix("pub(crate) ") {
+                (Visibility::PubCrate, rest.trim_start())
+            } else if let Some(rest) = rest.strip_prefix("pub ") {
+                (Visibility::Pub, rest.trim_start())
+            } else {
+                (Visibility::Private, rest)
+            };
+
+            let (is_unsafe, rest) = if let Some(rest) = rest.strip_prefix("unsafe ") {
+                (true, rest.trim_start())
+            } else {
+                (false, rest)
+            };
+
+            let (dik, rest) = if let Some(rest) = rest.strip_prefix("fn ")  { (DocItemKind::Fn,     rest.trim_start()) }
+            else if let Some(rest) = rest.strip_prefix("const ")            { (DocItemKind::Const,  rest.trim_start()) }
+            else if let Some(rest) = rest.strip_prefix("impl ")             { (DocItemKind::Impl,   rest.trim_start()) }
+            else if let Some(rest) = rest.strip_prefix("macro_rules! ")     { (DocItemKind::Macro,  rest.trim_start()) }
+            else if let Some(rest) = rest.strip_prefix("mod ")              { (DocItemKind::Mod,    rest.trim_start()) }
+            else if let Some(rest) = rest.strip_prefix("struct ")           { (DocItemKind::Struct, rest.trim_start()) }
+            else if let Some(rest) = rest.strip_prefix("trait ")            { (DocItemKind::Trait,  rest.trim_start()) }
+            else if let Some(rest) = rest.strip_prefix("type ")             { (DocItemKind::Type,   rest.trim_start()) }
+            else if rest.contains(":") && rest.contains(",") {
+                // struct item
+                s.on_comment_end(path, idx);
+                continue;
+            } else if rest.ends_with(r#": Option<unsafe extern "system" fn ("#) {
+                // struct fn item
+                s.on_comment_end(path, idx);
+                continue;
+            } else {
+                error!(at: path, line: no, "expected const, fn, impl, mod, or struct for documented item");
+                s.errors = true;
+                s.arguments.clear();
+                s.on_comment_end(path, idx);
+                continue;
+            };
+
+            if is_unsafe && !s.safety_line_no.is_some() {
+                error!(at: path, line: idx, "doc comment missing `### Safety` section");
+                s.errors = true;
+            }
+
+            match dik {
+                DocItemKind::Fn     => s.arguments.clear(), // XXX
+                DocItemKind::Macro  => s.arguments.clear(), // XXX
+                _                   => {},
+            }
+
+            // XXX
+            let _ = rest;
+
             s.on_comment_end(path, idx);
             continue
         } else {
@@ -128,6 +254,12 @@ fn scan_file_doc_comments(path: &Path, text: &str) -> Result<(), ()> {
                         },
                     };
 
+                    match h3 {
+                        H3::Arguments   => s.arguments_line_no  = Some(no),
+                        H3::Safety      => s.safety_line_no     = Some(no),
+                        _other          => {},
+                    }
+
                     if let Some(prev_h3) = s.current_h3 {
                         if prev_h3 >= h3 {
                             error!(at: path, line: no, "Expected `{}` to come before `### {}`", comment, prev_h3.as_str());
@@ -148,7 +280,21 @@ fn scan_file_doc_comments(path: &Path, text: &str) -> Result<(), ()> {
                 } else if let Some(li) = comment.strip_prefix("*   ") {
                     match s.current_h3 {
                         Some(H3::Arguments) => {
-                            if !li.starts_with("`") {
+                            if let Some(li) = li.strip_prefix("`") {
+                                let name_end = match li.bytes().position(|b| b == b'`') {
+                                    Some(n) => n,
+                                    None => {
+                                        error!(at: path, line: no, "Argument name missing closing backtick");
+                                        s.errors = true;
+                                        continue;
+                                    },
+                                };
+                                let (name, li) = li.split_at(name_end);
+                                let li = &li[1..].trim_start();
+                                let li = li.strip_prefix("-").unwrap_or(li);
+                                let _desc = li.trim_start();
+                                s.arguments.push(name.into());
+                            } else {
                                 error!(at: path, line: no, "Quote argument names with `backticks`");
                                 s.errors = true;
                             }
@@ -167,6 +313,8 @@ fn scan_file_doc_comments(path: &Path, text: &str) -> Result<(), ()> {
         }
     }
 
+    s.on_comment_end(path, last_line_no);
+
     if s.errors { Err(()) } else { Ok(()) }
 }
 
@@ -177,6 +325,7 @@ enum H3 {
     Safety,
     Usage,
     Arguments,
+    Panics,
     Errors,
     Returns,
     Examples,
@@ -192,6 +341,7 @@ impl FromStr for H3 {
             "Safety"    => Ok(H3::Safety),
             "Usage"     => Ok(H3::Usage),
             "Arguments" => Ok(H3::Arguments),
+            "Panics"    => Ok(H3::Panics),
             "Errors"    => Ok(H3::Errors),
             "Returns"   => Ok(H3::Returns),
             "Example"   => Ok(H3::Examples),
@@ -210,6 +360,7 @@ impl H3 {
             H3::Safety      => "Safety",
             H3::Usage       => "Usage",
             H3::Arguments   => "Arguments",
+            H3::Panics      => "Panics",
             H3::Errors      => "Errors",
             H3::Returns     => "Returns",
             H3::Examples    => "Examples",
@@ -218,4 +369,27 @@ impl H3 {
             H3::Remarks     => "Remarks",
         }
     }
+}
+
+
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+enum Visibility {
+    Pub,
+    PubCrate,
+    Private,
+}
+
+
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+enum DocItemKind {
+    Const,
+    Fn,
+    Impl,
+    Macro,
+    Mod,
+    Struct,
+    Trait,
+    Type, // possibly an associated type, possibly a free type
 }
