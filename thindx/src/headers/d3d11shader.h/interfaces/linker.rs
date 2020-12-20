9b642d832a6e14f12403a9190040a3c0a3cd0e96
MaulingMonkey <git@maulingmonkey.com>
2020-12-20 12:43:03 -0800

    d3d11::Linker::add_clip_plane_from_cbuffer:  test/doc more edge cases, avoid scrollbar

--- thindx/src/headers/d3d11shader.h/interfaces/linker.rs
+++ thindx/src/headers/d3d11shader.h/interfaces/linker.rs
@@ -36,6 +36,8 @@ impl Linker {
     /// ### Errors
     /// *   [E::INVALIDARG]     - If `cbuffer_slot` >= the maximum number of cbuffer slots (`15` on my machine)
     /// *   [E::INVALIDARG]     - If `cbuffer_entry` >= the maximum number of cbuffer entries (`4096` on my machine)
+    /// *   [E::INVALIDARG]     - If the slot + entry was already added
+    /// *   [E::FAIL]           - If trying to add too many clip planes (e.g. 6 were already added, and you're attempting to add a 7th)
     ///
     /// ### Example
     /// ```rust
@@ -43,30 +45,44 @@ impl Linker {
     /// let d3dc = d3d::Compiler::new(47).unwrap();
     /// let linker : d3d11::Linker = d3dc.create_linker().unwrap();
     ///
-    /// // https://docs.microsoft.com/en-us/windows/win32/direct3d11/overviews-direct3d-11-resources-buffers-intro#constant-buffer
-    /// // "Each shader stage allows up to 15 shader-constant buffers; each buffer can hold up to 4096 constants."
+    /// let add_clip_plane = |slot, entry| linker
+    ///     .add_clip_plane_from_cbuffer(slot, entry)
+    ///     .map_err(|e| e.kind());
     ///
-    /// assert_eq!(linker.add_clip_plane_from_cbuffer(14, 0).err().map(|err| err.kind()), None); // OK
-    /// assert_eq!(linker.add_clip_plane_from_cbuffer(15, 0).err().map(|err| err.kind()), Some(E::INVALIDARG));
+    /// assert_eq!(add_clip_plane(0, 0), Ok(())); // 1
+    /// assert_eq!(add_clip_plane(0, 0), Err(E::INVALIDARG)); // already added
     ///
-    /// assert_eq!(linker.add_clip_plane_from_cbuffer(0, 4095).err().map(|err| err.kind()), None); // OK
-    /// assert_eq!(linker.add_clip_plane_from_cbuffer(0, 4096).err().map(|err| err.kind()), Some(E::INVALIDARG));
+    /// // "Each shader stage allows up to 15 shader-constant buffers; [...]"
+    /// assert_eq!(add_clip_plane(1,  0), Ok(())); // 2
+    /// assert_eq!(add_clip_plane(14, 0), Ok(())); // 3
+    /// assert_eq!(add_clip_plane(15, 0), Err(E::INVALIDARG));
+    ///
+    /// // "[...] each buffer can hold up to 4096 constants."
+    /// assert_eq!(add_clip_plane(0, 1   ), Ok(())); // 4
+    /// assert_eq!(add_clip_plane(0, 4095), Ok(())); // 5
+    /// assert_eq!(add_clip_plane(0, 4096), Err(E::INVALIDARG));
     /// #
-    /// # assert_eq!(Some(E::INVALIDARG), linker.add_clip_plane_from_cbuffer(!0, 0).err().map(|err| err.kind()));
-    /// # assert_eq!(Some(E::INVALIDARG), linker.add_clip_plane_from_cbuffer(0, !0).err().map(|err| err.kind()));
-    /// # assert_eq!(Some(E::INVALIDARG), linker.add_clip_plane_from_cbuffer(0, !0-1).err().map(|err| err.kind()));
-    /// # assert_eq!(Some(E::INVALIDARG), linker.add_clip_plane_from_cbuffer(0, !0-4).err().map(|err| err.kind()));
-    /// # assert_eq!(Some(E::INVALIDARG), linker.add_clip_plane_from_cbuffer(0, !0-10).err().map(|err| err.kind()));
-    /// # assert_eq!(Some(E::INVALIDARG), linker.add_clip_plane_from_cbuffer(0, !0-100).err().map(|err| err.kind()));
-    /// # assert_eq!(Some(E::INVALIDARG), linker.add_clip_plane_from_cbuffer(!0-1, 0).err().map(|err| err.kind()));
-    /// # assert_eq!(Some(E::INVALIDARG), linker.add_clip_plane_from_cbuffer(!0-4, 0).err().map(|err| err.kind()));
-    /// # assert_eq!(Some(E::INVALIDARG), linker.add_clip_plane_from_cbuffer(!0-10, 0).err().map(|err| err.kind()));
-    /// # assert_eq!(Some(E::INVALIDARG), linker.add_clip_plane_from_cbuffer(!0-100, 0).err().map(|err| err.kind()));
-    /// # assert_eq!(Some(E::INVALIDARG), linker.add_clip_plane_from_cbuffer(!0, !0).err().map(|err| err.kind()));
+    /// # assert_eq!(Err(E::INVALIDARG), add_clip_plane(!0, 0));
+    /// # assert_eq!(Err(E::INVALIDARG), add_clip_plane(0, !0));
+    /// # assert_eq!(Err(E::INVALIDARG), add_clip_plane(0, !0-1));
+    /// # assert_eq!(Err(E::INVALIDARG), add_clip_plane(0, !0-4));
+    /// # assert_eq!(Err(E::INVALIDARG), add_clip_plane(0, !0-10));
+    /// # assert_eq!(Err(E::INVALIDARG), add_clip_plane(0, !0-100));
+    /// # assert_eq!(Err(E::INVALIDARG), add_clip_plane(!0-1, 0));
+    /// # assert_eq!(Err(E::INVALIDARG), add_clip_plane(!0-4, 0));
+    /// # assert_eq!(Err(E::INVALIDARG), add_clip_plane(!0-10, 0));
+    /// # assert_eq!(Err(E::INVALIDARG), add_clip_plane(!0-100, 0));
+    /// # assert_eq!(Err(E::INVALIDARG), add_clip_plane(!0, !0));
+    ///
+    /// // "... you provide a list of up to 6 float4 constants that define the plane coefficients
+    /// // for each active clip plane."
+    /// assert_eq!(add_clip_plane(3, 1), Ok(())); // 6th successful clip plane added to linker
+    /// assert_eq!(add_clip_plane(3, 2), Err(E::FAIL)); // 7th clip plane exceeds limits
     /// ```
     ///
     /// ### See Also
-    /// *   [User clip planes on feature level 9 hardware](https://docs.microsoft.com/en-us/windows/win32/direct3dhlsl/user-clip-planes-on-10level9)
+    /// *   [User clip planes on feature level 9 hardware](https://docs.microsoft.com/en-us/windows/win32/direct3dhlsl/user-clip-planes-on-10level9) (clip plane limit)
+    /// *   [Introduction to Buffers in Direct3D 11:  Constant Buffer](https://docs.microsoft.com/en-us/windows/win32/direct3d11/overviews-direct3d-11-resources-buffers-intro#constant-buffer) (cbuffer limits quoted in example)
     pub fn add_clip_plane_from_cbuffer(&self, cbuffer_slot: u32, cbuffer_entry: u32) -> Result<(), Error> {
         let hr = unsafe { self.0.AddClipPlaneFromCBuffer(cbuffer_slot, cbuffer_entry) };
         Error::check("ID3D11Linker::AddClipPlaneFromCBuffer", hr)
