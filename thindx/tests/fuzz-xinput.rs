7bb016a2e0c395abe51eed725232d30cacb7de41
MaulingMonkey <git@maulingmonkey.com>
2022-01-11 05:02:24 -0800

    fuzz-xinput: create a mixture of STA and MTA apartments

--- thindx/tests/fuzz-xinput.rs
+++ thindx/tests/fuzz-xinput.rs
@@ -8,7 +8,8 @@ use thindx::*;
 use mmrbi::*;
 
 use winapi::shared::winerror::*;
-use winapi::um::objbase::CoInitialize;
+use winapi::um::combaseapi::*;
+use winapi::um::objbase::*;
 
 use std::ptr::null_mut;
 use std::sync::Arc;
@@ -29,7 +30,7 @@ macro_rules! fuzz {
     let should_init_com     = Arc::new(AtomicBool::new(false));
     let continue_running    = Arc::new(AtomicBool::new(true));
 
-    let threads = (0 .. 8).map(|_|{
+    let threads = (0 .. 8).map(|thread_idx|{
         let should_init_com     = should_init_com.clone();
         let continue_running    = continue_running.clone();
         std::thread::spawn(move || {
@@ -38,7 +39,7 @@ macro_rules! fuzz {
             while !initialized_com || continue_running.load(Relaxed) {
                 if !initialized_com && should_init_com.load(Relaxed) {
                     initialized_com = true;
-                    initialize_com_for_the_first_time_on_this_thread();
+                    initialize_com_for_the_first_time_on_this_thread(thread_idx & 1 != 0);
                 }
 
                 for i in 0 ..= 100 { xinput::enable(i>>2 & 1 != 0) } // no error code, no xuser
@@ -137,11 +138,11 @@ macro_rules! fuzz {
     for thread in threads { thread.join().unwrap() }
 }
 
-fn initialize_com_for_the_first_time_on_this_thread() {
-    match unsafe { CoInitialize(null_mut()) } {
+fn initialize_com_for_the_first_time_on_this_thread(mta: bool) {
+    match unsafe { CoInitializeEx(null_mut(), if mta { COINIT_MULTITHREADED } else { COINIT_APARTMENTTHREADED }) } {
         S_OK    => {},
-        S_FALSE => fatal!("CoInitialize(nullptr) returned S_FALSE: COM was already initialized on this thread!  Tests were trying to ensure xinput is sound to use pre-COM..."),
-        err     => fatal!("CoInitialize(nullptr) returned 0x{:08X} (unexpected {})", err, if SUCCEEDED(err) { "success code" } else { "error" }),
+        S_FALSE => fatal!("CoInitializeEx(nullptr, ...) returned S_FALSE: COM was already initialized on this thread!  Tests were trying to ensure xinput is sound to use pre-COM..."),
+        err     => fatal!("CoInitializeEx(nullptr, ...) returned 0x{:08X} (unexpected {})", err, if SUCCEEDED(err) { "success code" } else { "error" }),
     }
 }
 
