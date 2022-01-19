1f2f114018719985f5f93f691a62cc222e5bd135
MaulingMonkey <git@maulingmonkey.com>
2022-01-18 18:14:51 -0800

    xinput.h/structures/gamepad.rs: add missing constants
    I've decided that deadzone/pairing fns are arguably beyond the scope of thindx.  It's also likely you'll want to handle such things at a higher level, agnostic of the underlying gamepad implementation provider, perhaps built on an abstraction - thindx providing built-in behavior would just be an unnecessary distraction and red herring.

--- thindx/src/headers/xinput.h/structures/gamepad.rs
+++ thindx/src/headers/xinput.h/structures/gamepad.rs
@@ -1,4 +1,5 @@
-use crate::xinput::*;
+use crate::*;
+use winapi::um::xinput::*;
 use bytemuck::{Pod, Zeroable};
 
 
@@ -7,19 +8,26 @@ use bytemuck::{Pod, Zeroable};
 /// XINPUT_GAMEPAD
 ///
 /// Describes the state of an Xbox 360 controller.
+///
+/// ### See Also
+///
+/// *   [examples::d3d9_02_xinput]
+/// *   [Doing thumbstick dead zones right](https://web.archive.org/web/20141025190105/https://www.third-helix.com/2013/04/12/doing-thumbstick-dead-zones-right.html)
+///     (**strongly** recommended reading!)
+/// *   [Xbox 360 controller - Layout](https://en.wikipedia.org/wiki/Xbox_360_controller#Layout) - Wikipedia
 #[derive(Clone, Copy, Debug)]
 #[derive(Default, Pod, Zeroable)]
 #[repr(C)] pub struct Gamepad {
 
     /// Buttons of the gamepad that are currently held.
-    pub buttons: Buttons,
+    pub buttons: xinput::Buttons,
 
     /// Analog trigger behind the left side of the gamepad.
     ///
     /// | Value | Description   |
     /// | -----:| ------------- |
     /// |     0 | Not held
-    /// |    30 | `XINPUT_GAMEPAD_TRIGGER_THRESHOLD`
+    /// |    30 | [xinput::Gamepad::TRIGGER_THRESHOLD]
     /// |   255 | Fully held
     pub left_trigger: u8,
 
@@ -28,7 +36,7 @@ use bytemuck::{Pod, Zeroable};
     /// | Value | Description   |
     /// | -----:| ------------- |
     /// |     0 | Not held
-    /// |    30 | `XINPUT_GAMEPAD_TRIGGER_THRESHOLD`
+    /// |    30 | [xinput::Gamepad::TRIGGER_THRESHOLD]
     /// |   255 | Fully held
     pub right_trigger: u8,
 
@@ -37,9 +45,9 @@ use bytemuck::{Pod, Zeroable};
     /// | Value     | Description   |
     /// | ---------:| ------------- |
     /// |    -32768 | Fully left
-    /// |     -7849 | `-XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE`
-    /// |         0 | Horizontally centered
-    /// |     +7849 | `+XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE`
+    /// |     -7849 | - [xinput::Gamepad::LEFT_THUMB_DEADZONE]
+    /// |         0 | Centered
+    /// |     +7849 | + [xinput::Gamepad::LEFT_THUMB_DEADZONE]
     /// |    +32767 | Fully right
     pub left_thumb_x: i16,
 
@@ -48,9 +56,9 @@ use bytemuck::{Pod, Zeroable};
     /// | Value     | Description   |
     /// | ---------:| ------------- |
     /// |    -32768 | Fully down
-    /// |     -7849 | `-XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE`
-    /// |         0 | Horizontally centered
-    /// |     +7849 | `+XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE`
+    /// |     -7849 | - [xinput::Gamepad::LEFT_THUMB_DEADZONE]
+    /// |         0 | Centered
+    /// |     +7849 | + [xinput::Gamepad::LEFT_THUMB_DEADZONE]
     /// |    +32767 | Fully up
     pub left_thumb_y: i16,
 
@@ -59,9 +67,9 @@ use bytemuck::{Pod, Zeroable};
     /// | Value     | Description   |
     /// | ---------:| ------------- |
     /// |    -32768 | Fully left
-    /// |     -8689 | `-XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE`
-    /// |         0 | Horizontally centered
-    /// |     +8689 | `+XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE`
+    /// |     -8689 | - [xinput::Gamepad::RIGHT_THUMB_DEADZONE]
+    /// |         0 | Centered
+    /// |     +8689 | + [xinput::Gamepad::RIGHT_THUMB_DEADZONE]
     /// |    +32767 | Fully right
     pub right_thumb_x: i16,
 
@@ -70,15 +78,38 @@ use bytemuck::{Pod, Zeroable};
     /// | Value     | Description   |
     /// | ---------:| ------------- |
     /// |    -32768 | Fully down
-    /// |     -8689 | `-XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE`
-    /// |         0 | Horizontally centered
-    /// |     +8689 | `+XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE`
+    /// |     -8689 | - [xinput::Gamepad::RIGHT_THUMB_DEADZONE]
+    /// |         0 | Centered
+    /// |     +8689 | + [xinput::Gamepad::RIGHT_THUMB_DEADZONE]
     /// |    +32767 | Fully up
     pub right_thumb_y: i16,
 }
 
-// TODO: deadzone fns?
-// TODO: pairing fns?
+impl Gamepad {
+    /// An optional default threshhold to compare [Gamepad::left_trigger] or [Gamepad::right_trigger] to, before which you might avoid registering a trigger pull.
+    ///
+    /// In my experience, Xbox 360 controllers all perfectly report 0 trigger when untouched, so this is somewhat optional.
+    /// On the other hand, there's no guarantee third party XInput compatible controllers behave the same, so perhaps you should use such a constant!
+    pub const TRIGGER_THRESHOLD : u8 = XINPUT_GAMEPAD_TRIGGER_THRESHOLD;
+
+    /// A default deadzone magnitude for the left thumbstick.
+    ///
+    /// You might ignore [Gamepad::left_thumb_x] / [Gamepad::left_thumb_y] when they're within this magnitude of (0,0).
+    /// While this is generally large enough to avoid drift on new controllers, I have seen more well used controllers rest outside of this magnitude, resulting in suprising drift.
+    /// For maximum flexibility, consider making the user's deadzone customizeable, and perhaps using a higher threshhold for triggering game UI navigation.
+    ///
+    /// I also **strongly** recommend reading [Doing thumbstick dead zones right](https://web.archive.org/web/20141025190105/https://www.third-helix.com/2013/04/12/doing-thumbstick-dead-zones-right.html).
+    pub const LEFT_THUMB_DEADZONE : i16 = XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE;
+
+    /// A default deadzone magnitude for the right thumbstick.
+    ///
+    /// You might ignore [Gamepad::right_thumb_x] / [Gamepad::right_thumb_y] when they're within this magnitude of (0,0).
+    /// While this is generally large enough to avoid drift on new controllers, I have seen more well used controllers rest outside of this magnitude, resulting in suprising drift.
+    /// For maximum flexibility, consider making the user's deadzone customizeable, and perhaps using a higher threshhold for triggering game UI navigation.
+    ///
+    /// I also **strongly** recommend reading [Doing thumbstick dead zones right](https://web.archive.org/web/20141025190105/https://www.third-helix.com/2013/04/12/doing-thumbstick-dead-zones-right.html).
+    pub const RIGHT_THUMB_DEADZONE : i16 = XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE;
+}
 
 test_layout_only! {
     Gamepad => winapi::um::xinput::XINPUT_GAMEPAD {
