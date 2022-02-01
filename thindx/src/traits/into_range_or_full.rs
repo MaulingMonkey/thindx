e8e6539000e369aa3891d2675ac328fb57ab0256
MaulingMonkey <git@maulingmonkey.com>
2022-02-01 15:25:07 -0800

    doc: IntoXyzOrFull: fuller types

--- thindx/src/traits/into_range_or_full.rs
+++ thindx/src/traits/into_range_or_full.rs
@@ -1,10 +1,10 @@
-use crate::d3d::{Box, Rect};
+use crate::d3d;
 
 use std::ops::{Range, RangeFull};
 
 
 
-/// [`Range`] or [`..`](RangeFull)
+/// [`std::ops::Range`] or [`..`](RangeFull)
 pub trait IntoRangeOrFull<T> {
     /// ### Returns
     /// *   Some([Range]) to indicate a subrange
@@ -12,27 +12,27 @@ pub trait IntoRangeOrFull<T> {
     fn into_range(self) -> Option<Range<T>>;
 }
 
-/// [`Rect`] or [`..`](RangeFull)
+/// [`d3d::Rect`] or [`..`](RangeFull)
 pub trait IntoRectOrFull {
     /// ### Returns
-    /// *   Some([Rect]) to indicate a subrect
+    /// *   Some([d3d::Rect]) to indicate a subrect
     /// *   [None] to indicate [RangeFull]
-    fn into_rect(self) -> Option<Rect>;
+    fn into_rect(self) -> Option<d3d::Rect>;
 }
 
-/// [`Box`] or [`..`](RangeFull)
+/// [`d3d::Box`] or [`..`](RangeFull)
 pub trait IntoBoxOrFull {
     /// ### Returns
-    /// *   Some([Box]) to indicate a subbox
+    /// *   Some([d3d::Box]) to indicate a subbox
     /// *   [None] to indicate [RangeFull]
-    fn into_box(self) -> Option<Box>;
+    fn into_box(self) -> Option<d3d::Box>;
 }
 
 impl<T> IntoRangeOrFull<T> for Range<T>     { fn into_range(self) -> Option<Range<T>> { Some(self) } }
 impl<T> IntoRangeOrFull<T> for RangeFull    { fn into_range(self) -> Option<Range<T>> { None } }
 
-impl<T: Into<Rect>> IntoRectOrFull for T    { fn into_rect(self) -> Option<Rect> { Some(self.into()) } }
-impl IntoRectOrFull for RangeFull           { fn into_rect(self) -> Option<Rect> { None } }
+impl<T: Into<d3d::Rect>> IntoRectOrFull for T   { fn into_rect(self) -> Option<d3d::Rect> { Some(self.into()) } }
+impl IntoRectOrFull for RangeFull               { fn into_rect(self) -> Option<d3d::Rect> { None } }
 
-impl<T: Into<Box>> IntoBoxOrFull for T      { fn into_box(self) -> Option<Box> { Some(self.into()) } }
-impl IntoBoxOrFull for RangeFull            { fn into_box(self) -> Option<Box> { None } }
+impl<T: Into<d3d::Box>> IntoBoxOrFull for T     { fn into_box(self) -> Option<d3d::Box> { Some(self.into()) } }
+impl IntoBoxOrFull for RangeFull                { fn into_box(self) -> Option<d3d::Box> { None } }
